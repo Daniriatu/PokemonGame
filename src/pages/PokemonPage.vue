4a5fff30bafb4c4aecd9a2bbd6f1660ac8d6bcf2
Rongji Tu <daniriaatuu@gmail.com>
2023-02-22 16:39:35 +0100

    NewGame Fixed

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import PokemonPicture from '../components/PokemonPicture.vue';
-import PokemonOptions from '../components/PokemonOptions.vue';
-import getPokemonOptions from '@/helpers/getPokemonOptions';
-import type { Pokemon } from '../interfaces/pokemon';
-import { ref } from 'vue';
+import PokemonPicture from "../components/PokemonPicture.vue";
+import PokemonOptions from "../components/PokemonOptions.vue";
+import getPokemonOptions from "@/helpers/getPokemonOptions";
+import type { Pokemon } from "../interfaces/pokemon";
+import { ref } from "vue";
 
 const pokemonArr = ref<Pokemon[]>([]);
 
@@ -16,50 +16,52 @@ const showAnswer = ref(false);
 const message = ref("");
 
 const mixPokemonArray = async () => {
-    pokemonArr.value = await getPokemonOptions();
-    const randomInt = Math.floor(Math.random() * 4);
-    pokemon.value = pokemonArr.value[randomInt];
-    // console.log(randomInt);
-    // console.log(pokemonArr.value);
-}
+  pokemonArr.value = await getPokemonOptions();
+  const randomInt = Math.floor(Math.random() * 4);
+  pokemon.value = pokemonArr.value[randomInt];
+  // console.log(randomInt);
+  // console.log(pokemonArr.value);
+};
 
 // getPokemonOptions();
 mixPokemonArray();
 
 const checkAnswer = (selectedId: number) => {
-    if (!pokemon.value) return;
+  if (!pokemon.value) return;
 
-    showAnswer.value = true;
+  showAnswer.value = true;
 
-    showPokemon.value = true;
+  showPokemon.value = true;
 
-    if (selectedId === pokemon.value.id) {
-        message.value = `Correcto ${pokemon.value.name}`
-    } else {
-        message.value = `Oops, era, ${pokemon.value.name}`
-    }
-
-}
+  if (selectedId === pokemon.value.id) {
+    message.value = `Correcto ${pokemon.value.name}`;
+  } else {
+    message.value = `Oops, era, ${pokemon.value.name}`;
+  }
+};
 
 const newGame = () => {
-    // console.log("Iniciar");
-    window.location.reload();
-}
+  // console.log("Iniciar");
+  // window.location.reload();
+  showPokemon.value = false;
+  showAnswer.value = false;
+  pokemonArr.value = [];
+  pokemon.value = undefined;
 
+  mixPokemonArray();
+};
 </script>
 <template>
-    <h1 v-if="!pokemon">Espere por favor ...</h1>
-    <div v-else>
-        <h1>¿Quién es este Pokemón?</h1>
-        <PokemonPicture :pokemonId="pokemon.id" :showPokemon="showPokemon" />
-        <PokemonOptions :pokemons="pokemonArr" @selectionPokemon="checkAnswer" />
-        <div v-if="showAnswer">
-            <h2 class="fade-in">{{ message }}</h2>
-            <button @click="newGame">
-                Nuevo juego
-            </button>
-        </div>
+  <h1 v-if="!pokemon">Espere por favor ...</h1>
+  <div v-else>
+    <h1>¿Quién es este Pokemón?</h1>
+    <PokemonPicture :pokemonId="pokemon.id" :showPokemon="showPokemon" />
+    <PokemonOptions :pokemons="pokemonArr" @selectionPokemon="checkAnswer" />
+    <div v-if="showAnswer">
+      <h2 class="fade-in">{{ message }}</h2>
+      <button @click="newGame">Nuevo juego</button>
     </div>
+  </div>
 </template>
 
 <style scoped></style>
